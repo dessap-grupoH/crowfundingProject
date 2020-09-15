a84954882aadb137587172791de73c78ef2a16d1
LM-Randomize <lcmurc@gmail.com>
2020-09-15 01:43:19 -0300

    Fix variable codacy...
    AGAIN.

--- Backend/src/main/kotlin/ar/unq/edu/desapp/grupoH/model/Donation.kt
+++ Backend/src/main/kotlin/ar/unq/edu/desapp/grupoH/model/Donation.kt
@@ -1,8 +1,6 @@
 package ar.unq.edu.desapp.grupoH.model
 
 import java.time.LocalDate
-import java.util.*
 
 class Donation(val date: LocalDate?, val paymentMethod: PaymentMethod,
-               val amount: Int, val to: Town, val from: String, val comment: String) {
-}
+               val amount: Int, val to: Town, val from: String, val comment: String)
--- Backend/src/main/kotlin/ar/unq/edu/desapp/grupoH/model/Point.kt
+++ Backend/src/main/kotlin/ar/unq/edu/desapp/grupoH/model/Point.kt
@@ -1,8 +1,5 @@
 package ar.unq.edu.desapp.grupoH.model
 
-import ar.unq.edu.desapp.grupoH.model.CrowdfundingProject
-import ar.unq.edu.desapp.grupoH.model.User
 import java.time.LocalDate
 
-class Point(val user: User, val comment: String, val value: Int, val date: LocalDate, val project: CrowdfundingProject) {
-}
+class Point(val user: User, val comment: String, val value: Int, val date: LocalDate, val project: CrowdfundingProject)
