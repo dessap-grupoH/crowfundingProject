0d1b8d7d3f2d78b88e42af8f7ff09e468247a934
LM-Randomize <lcmurc@gmail.com>
2020-09-17 02:31:36 -0300

    Empty class sin llaves..

--- Backend/src/main/kotlin/ar/unq/edu/desapp/grupoH/model/user/User.kt
+++ Backend/src/main/kotlin/ar/unq/edu/desapp/grupoH/model/user/User.kt
@@ -1,4 +1,3 @@
 package ar.unq.edu.desapp.grupoH.model.user
 
-abstract class User(val username: String, val password: String, var email: String) {
-}
+abstract class User(val username: String, val password: String, var email: String)
