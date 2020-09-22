31406f177bd81bd4ff0c9b6f233f416135485b48
Rodrigo <rodrigogarciaunq@gmail.com>
2020-09-21 21:32:58 -0300

    arreglo tag

--- Backend/src/main/kotlin/ar/unq/edu/desapp/grupoH/model/Donation.kt
+++ Backend/src/main/kotlin/ar/unq/edu/desapp/grupoH/model/Donation.kt
@@ -3,5 +3,4 @@ package ar.unq.edu.desapp.grupoH.model
 import ar.unq.edu.desapp.grupoH.model.user.DonorUser
 import java.time.LocalDate
 
-class Donation(val date: LocalDate?, val paymentMethod: PaymentMethod,
-               val amount: Int, val projectTo: CrowdfundingProject, val from: DonorUser, val comment: String)
+class Donation(val date: LocalDate?, val paymentMethod: PaymentMethod, val amount: Int, val projectTo: CrowdfundingProject, val from: DonorUser, val comment: String)
