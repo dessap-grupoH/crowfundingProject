8935c85a562624de227f28419d644502d87a5eff
Rodrigo <rodrigogarciaunq@gmail.com>
2020-10-19 00:52:47 -0300

    impl axios, endpoints fetch

---
+++ Frontend/src/Utils/Api.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const HOST_URL = "http://localHost:8080";
+const USER_URL = "/api/user";
+const TOWNS_URL = "/api/towns";
+const DONATION_URL = "/api/donation";
+const PROJECT_URL = "/api/projects  "
+
+
+const fetchUser = (userID) =>
+  axios.get(`${HOST_URL}${USER_URL}?id=${userID}`);
+
+const fetchTowns = () =>
+  axios.get(`${HOST_URL}${TOWNS_URL}`);
+
+const fetchProjects = () =>
+  axios.get(`${HOST_URL}${PROJECT_URL}`);
+
+const fetchOpenProjects = () =>
+  axios.get(`${HOST_URL}${PROJECT_URL}/open`);
+
+const fetchProjectDetail = (proejctID) =>
+  axios.get(`${HOST_URL}${PROJECT_URL}/details?id=${proejctID}`);
+
+const postDonation = (donation) =>
+  axios.post(`${HOST_URL}${DONATION_URL}`, donation);
+
+
+export {
+  fetchUser,
+  fetchTowns,
+  fetchProjects,
+  fetchOpenProjects,
+  fetchProjectDetail,
+  postDonation
+};
