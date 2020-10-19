ae9effa4fb0bf3872bcd0a42f68b4d80cc0fdb72
Rodrigo <rodrigogarciaunq@gmail.com>
2020-10-19 20:00:59 -0300

    react i18n implemented, deleted next and i18next jeje

--- Frontend/src/Pages/Header.js
+++ Frontend/src/Pages/Header.js
@@ -1,34 +1,39 @@
 import React from 'react';
-import ParticlesBg  from "particles-bg";
+import ParticlesBg from "particles-bg";
 import Navbar from "../Components/Navbar";
 import "../Pages/Header.css"
+import { useTranslation } from "react-i18next"
 
-const Header = () => (
-   <header id="intro">
-      <ParticlesBg type="cobweb" bg={true} />
+const Header = () => {
 
-      <Navbar
-         refIntro = "#intro"
-         refProjects = "#currentProjects"
-         refDonate = "#donate"
-         refMyDonations = "#myDonations"
-      />
+   const [t, i18n] = useTranslation("global");
 
-      <div className="row banner">
-      
-         <div className="banner-text">
-            <h1 className="responsive-headline"> Proyecto solidario </h1>
-            <h3>
-               Este es un proyecto donde podras hacer donaciones a distintos barrios 
-               del pais que no posean internet hasta el momento, de esta forma podremos darle
-               una oportunidad de crecimiento importante.
+   return (
+      <header id="intro">
+         <ParticlesBg type="cobweb" bg={true} />
+
+         <Navbar
+            refIntro="#intro"
+            refProjects="#currentProjects"
+            refDonate="#donate"
+            refMyDonations="#myDonations"
+         />
+
+         <div className="row banner">
+
+            <div className="banner-text">
+               <h1 className="responsive-headline"> {t("header.crowdfunding-project")} </h1>
+               <h3>
+                  Este es un proyecto donde podras hacer donaciones a distintos barrios
+                  del pais que no posean internet hasta el momento, de esta forma podremos darle
+                  una oportunidad de crecimiento importante.
             </h3>
-            <hr />
+               <hr />
+            </div>
          </div>
-      </div>
 
-    </header>
-);
-
+      </header>
+   );
+}
 
 export default Header;
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -2,6 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import "./global.css"
+import "./global.css";
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next";
+import globalEs from "../src/Translations/Es/global.json";
+import globalEn from "../src/Translations/En/global.json";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: "es",
+  resources: {
+    es: {
+      global: globalEs
+    },
+    en: {
+      global: globalEn
+    }
+  }
+});
+
+ReactDOM.render(
+  <I18nextProvider i18n={i18next}>
+    <App />
+  </I18nextProvider>,
+  document.getElementById('root')
+);
